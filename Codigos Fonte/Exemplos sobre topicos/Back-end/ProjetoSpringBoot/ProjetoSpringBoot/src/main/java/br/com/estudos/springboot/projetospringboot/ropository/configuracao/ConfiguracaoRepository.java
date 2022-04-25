68ee047c9832a0797f7a368a14361cd05872bc08
Henrique Trajano de Araujo <henrique.trajano.a@gmail.com>
2022-04-25 20:11:39 -0300

    refatoracao-classe-de-configuracao-do-spring
    implementacao para utilizar o @Bean da classe ENtityManagerFactory gerado automaticamanete pelo Spring para gerar as instancias das classes GenericoRepository e GenericoService.

--- Codigos Fonte/Exemplos sobre topicos/Back-end/ProjetoSpringBoot/ProjetoSpringBoot/src/main/java/br/com/estudos/springboot/projetospringboot/ropository/configuracao/ConfiguracaoRepository.java
+++ Codigos Fonte/Exemplos sobre topicos/Back-end/ProjetoSpringBoot/ProjetoSpringBoot/src/main/java/br/com/estudos/springboot/projetospringboot/ropository/configuracao/ConfiguracaoRepository.java
@@ -3,30 +3,21 @@ package br.com.estudos.springboot.projetospringboot.ropository.configuracao;
 import br.com.estudos.springboot.projetospringboot.domain.comum.EntidadeComum;
 import br.com.estudos.springboot.projetospringboot.ropository.GenericoRepository;
 import br.com.estudos.springboot.projetospringboot.ropository.GenericoRepository.GenericoRepositoryBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
 
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 @Configuration
+@ComponentScan(value = "javax.persistence")
 public class ConfiguracaoRepository {
 
-    @Bean
-    public EntityManagerFactory entityManagerFactory(){
-        return Persistence.createEntityManagerFactory("jpa-unit");
-    }
-
-    @Bean
-    public EntityManager entityManagerBeanBuilder(EntityManagerFactory entityManagerFactoryBeanBuilder){
-        return entityManagerFactoryBeanBuilder.createEntityManager();
-    }
+    @Autowired
+    EntityManagerFactory entityManagerFactory;
 
     @Bean
     @Scope(value = "prototype")
-    public <E extends EntidadeComum, I> GenericoRepository<E, I> clienteRepositoryBeanBuilder(Class<E> entityClazz, Class<I> identificadorClazz){
-        return new GenericoRepositoryBuilder<>(entityClazz, identificadorClazz, entityManagerBeanBuilder(entityManagerFactory())).build();
+    public <E extends EntidadeComum, I> GenericoRepository<E, I> genericoRepositoryBeanBuilder(Class<E> entityClazz, Class<I> identificadorClazz){
+        return new GenericoRepositoryBuilder<>(entityClazz, identificadorClazz, entityManagerFactory.createEntityManager()).build();
     }
 }
