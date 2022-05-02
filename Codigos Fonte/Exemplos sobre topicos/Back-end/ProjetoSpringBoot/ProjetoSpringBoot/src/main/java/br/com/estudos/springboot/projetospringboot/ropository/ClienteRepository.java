f8f01deb807dcc0a6f8c403fca53cd6e3618966a
Henrique Trajano de Araujo <henrique.trajano.a@gmail.com>
2022-05-02 04:05:48 -0300

    validacao-customizada-para-email
    Regra de negocio: nao podem existir emails repetidos no banco de dados.
    
    - Implementacao do metodo findByEmail() na interface ClienteRepository, pelo padrao do nome findBy<nome-da-entidade> o spring implementa a busca autmaticamente.
    
    - insercao da regra de validacao na classe ClienteNovoDTOValidation

--- Codigos Fonte/Exemplos sobre topicos/Back-end/ProjetoSpringBoot/ProjetoSpringBoot/src/main/java/br/com/estudos/springboot/projetospringboot/ropository/ClienteRepository.java
+++ Codigos Fonte/Exemplos sobre topicos/Back-end/ProjetoSpringBoot/ProjetoSpringBoot/src/main/java/br/com/estudos/springboot/projetospringboot/ropository/ClienteRepository.java
@@ -4,7 +4,14 @@ import br.com.estudos.springboot.projetospringboot.domain.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+    /* O padrao de nomes findBy<nome-da-entidade> faz com que o spring
+        implemente automaticamento a busca pelo campo desejado */
+    @Transactional
+    Cliente findByEmail(String email);
 
 }
--- Codigos Fonte/Exemplos sobre topicos/Back-end/ProjetoSpringBoot/ProjetoSpringBoot/src/main/java/br/com/estudos/springboot/projetospringboot/service/validacao/ClienteNovoDTOValidation.java
+++ Codigos Fonte/Exemplos sobre topicos/Back-end/ProjetoSpringBoot/ProjetoSpringBoot/src/main/java/br/com/estudos/springboot/projetospringboot/service/validacao/ClienteNovoDTOValidation.java
@@ -3,7 +3,9 @@ package br.com.estudos.springboot.projetospringboot.service.validacao;
 import br.com.estudos.springboot.projetospringboot.domain.dto.ClienteNovoDTO;
 import br.com.estudos.springboot.projetospringboot.domain.enums.TipoCliente;
 import br.com.estudos.springboot.projetospringboot.resource.exceptions.FieldMensage;
+import br.com.estudos.springboot.projetospringboot.ropository.ClienteRepository;
 import br.com.estudos.springboot.projetospringboot.service.validacao.utils.ValidacaoCpfOuCnpj;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -11,6 +13,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteNovoDTOValidation implements ConstraintValidator<ClienteNovoDTOAnnotationValidation, ClienteNovoDTO> {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
 
     @Override
     public void initialize(ClienteNovoDTOAnnotationValidation constraintAnnotation) {
@@ -27,6 +32,8 @@ public class ClienteNovoDTOValidation implements ConstraintValidator<ClienteNovo
         else if(clienteNovoDTO.getTipo().equals(TipoCliente.PESSOA_FISICA.getId()) && ValidacaoCpfOuCnpj.isNotValidCpf(clienteNovoDTO.getCpfOuCnpj()))
             errors.add(new FieldMensage("cpfOuCnpj", "Cpf Invalido"));
 
+        if(clienteRepository.findByEmail(clienteNovoDTO.getEmail()) != null)
+            errors.add(new FieldMensage("email", "O email fornecido ja esta cadastrado"));
 
         for(FieldMensage msg : errors){
             constraintValidatorContext.disableDefaultConstraintViolation();
