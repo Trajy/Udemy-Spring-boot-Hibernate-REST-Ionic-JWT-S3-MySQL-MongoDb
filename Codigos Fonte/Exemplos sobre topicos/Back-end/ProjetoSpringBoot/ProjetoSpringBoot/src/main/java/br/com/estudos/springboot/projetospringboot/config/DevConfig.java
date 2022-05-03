3cb265eb91fb646810c65f10f1020ad95ef105ae
Henrique Trajano de Araujo <henrique.trajano.a@gmail.com>
2022-05-03 11:27:50 -0300

    criacao-profile-desenvolvimento
    ALTERACOES:
    
    - O banco de dados utilizado para o profile de testes foi alterado para o H2.
    
    - pom.mlx: adicionada a dependencia para o banco de dados H2.
    
    ARQUIVOS DE CONFIG:
    
    - application-dev.yml : contem as configuracoes que antes estavam em application-test.yml, o banco de dados utilizado no ambiente de desenvolvimento sera o mySQL.
    
    - application-test.yml: contem as configuracoes para o banco de dados H2.
    
    - application.yml: alteracao do profile setado para dev.
    
    - criacao da classe DevConfig: que que le a propriedade ddl-auto do arquivo de configuracoes somente se for create persiste os dados no banco para que nao sejam duplicados.

---
+++ Codigos Fonte/Exemplos sobre topicos/Back-end/ProjetoSpringBoot/ProjetoSpringBoot/src/main/java/br/com/estudos/springboot/projetospringboot/config/DevConfig.java
@@ -0,0 +1,34 @@
+package br.com.estudos.springboot.projetospringboot.config;
+
+import br.com.estudos.springboot.projetospringboot.service.DataBaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Optional;
+
+@Configuration
+@Profile("dev") // annotation para identificar que essa classe so deve ser utilizada ao setar o profile test no arquivo de configuracoes.
+public class DevConfig {
+
+    @Autowired
+    DataBaseService dataBaseService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String dataBaseInstanceStrategy;
+
+    @Bean
+    public boolean instanciateDataBase(){
+
+        if(!dataBaseInstanceStrategy.equals("create")) return false;
+
+        dataBaseService.instantiateTestDataBase();
+
+        // apenas para retornar algo, os beans nao podem retornar o primitivo void
+        return true;
+    }
+}
+
+
