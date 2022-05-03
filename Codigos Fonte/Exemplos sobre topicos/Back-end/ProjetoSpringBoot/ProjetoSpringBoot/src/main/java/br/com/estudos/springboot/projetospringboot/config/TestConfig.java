d3777c6ac85337fa890adf4c56dd4569ec021941
Henrique Trajano de Araujo <henrique.trajano.a@gmail.com>
2022-05-03 10:04:18 -0300

    criacao-profile-test
    ALTERACOES:
    
    - Os arquivos de configuracao foram alterados para o formato yml. O arquivo de configuracoes anterior foi mantido no projeto para consulta como applicationBackup.properties. Adicionado o Pdf Referente ao modulo 4 (Banco de dados MySQL e Implantacao no Heroku) do curso
    
    ARQUIVOS DE CONFIGURACAO:
    
    - applicarion.yml: as configuracoes do banco de dados foram removidas deste arquivo e movidas para o arquivo application-test.yml (o Spring identifica o profile pelo nome depois do traco), e adicionado a propriedade para selecionar o profile desejado.
    
    - application-test.yml: contem as configuracoes pertinentes ao ambiente de testes.
    
    CLASSES:
    
    - ProjetoSpringBootApplication: os dados inseridos no banco de dados ao iniciar a applicacao foram movidos para uma classe de servico especifica DataBaseService.
    
    - DatBaseService: contem os dados para serem inseridos no banco de dados e o metodo instantiateTestDataBase() que persiste os dados no banco.

---
+++ Codigos Fonte/Exemplos sobre topicos/Back-end/ProjetoSpringBoot/ProjetoSpringBoot/src/main/java/br/com/estudos/springboot/projetospringboot/config/TestConfig.java
@@ -0,0 +1,29 @@
+package br.com.estudos.springboot.projetospringboot.config;
+
+import br.com.estudos.springboot.projetospringboot.service.DataBaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Optional;
+
+@Configuration
+@Profile("test") // annotation para identificar que essa classe so deve ser utilizada ao setar o profile test no arquivo de configuracoes.
+public class TestConfig {
+
+    @Autowired
+    DataBaseService dataBaseService;
+
+    @Bean
+    public Optional<?> instanciateDataBase(){
+
+        dataBaseService.instantiateTestDataBase();
+
+        // apenas para retornar algo, os beans nao podem retornar o primitivo void
+        Optional<?> voyd = Optional.ofNullable(Void.TYPE);
+        return voyd;
+    }
+}
+
+
