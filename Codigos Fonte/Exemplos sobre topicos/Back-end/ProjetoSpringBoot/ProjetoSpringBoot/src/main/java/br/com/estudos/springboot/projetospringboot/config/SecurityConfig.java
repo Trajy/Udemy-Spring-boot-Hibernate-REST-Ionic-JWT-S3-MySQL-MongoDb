b96a7e8dceb4ebac7fff8850c3d91e3bfaf46a30
Henrique Trajano de Araujo <henrique.trajano.a@gmail.com>
2022-05-08 06:55:08 -0300

    implementacao-incial-spring-security
    ALTERACOES:
    
    - pom.xml: adicao das dependencias necessarias para trabalhar com Spring Security
    
    - application-dev.yml: adicao das configuracoes utilizadas na class JavaMailSender, pois o projeto da erro ao tentar subir (resolver de uma forma melhor posteriormente).
    
    - application.yml: alteracao para o ambiente de testes.
    
    - SecurityConfig: classe de configuracao para o Spring Security.

---
+++ Codigos Fonte/Exemplos sobre topicos/Back-end/ProjetoSpringBoot/ProjetoSpringBoot/src/main/java/br/com/estudos/springboot/projetospringboot/config/SecurityConfig.java
@@ -0,0 +1,76 @@
+package br.com.estudos.springboot.projetospringboot.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private Environment env;
+
+    // Array para guardar os endpoints que nao necessitao de autenticacao
+    private static final String[] PUBLIC_MATCHERS = {
+        "/h2/**"
+    };
+
+    /*
+        para os casos de categoria e cliente por exemplo, nao queremos deixar o acesso liberado
+        para todos os end-points, pois um usuario que nao e administrador do sistema nao pode,
+        inserir, alterar ou deletar nada na base de dados, ele pode apenas recuperar os dados,
+        para isto e feito uma limitacao para os metodos get.
+     */
+    private static final String[] PUBLIC_MATCHERS_GET = {
+        "/produtos/**",
+        "/categorias/**"
+    };
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+        /*
+            como o banco de dados H2 gera varios frames na tela em sua construcao e necessario liberar pelo framework
+         */
+        if(Arrays.asList(env.getActiveProfiles()).contains("test")){
+            httpSecurity.headers().frameOptions().disable();
+        }
+
+        /*
+          o metodo cors() procura se existe um bean do tipo CorsConfigurationSource e applica automaticamente ao HttpSecurity
+
+         .and().crsf().disable() desabilira protecao contra ataques do tipo csrf (nao e necessario para applicacoes que nao armazenao autenticacao por secao)
+        */
+        httpSecurity.cors().and().csrf().disable();
+
+        /*
+            permite apenas acesso aos endpoints declarados no array PUBLIC_MATCHER
+            o restante dos end-points solicita autenticacao.
+
+         */
+        httpSecurity.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll().
+            antMatchers(PUBLIC_MATCHERS).permitAll().
+            anyRequest().authenticated();
+
+        // para garantir que a politica de acessos sera stateless, ou seja nao ira armazenar a secao do usuario.
+        httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+}
