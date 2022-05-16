11325fee9c8cd5ddc9409c98622923825420ee72
Henrique Trajano de Araujo <henrique.trajano.a@gmail.com>
2022-05-16 10:27:24 -0300

    salvando-primeiro-arquivo-amazon-s3
    ALTERACOES:
    
    - pom.xml: adicao das depedencias asw-java-sdk que contem as classes necessarias para comunicacao com a API da aws, e commons.io que auxilia na manipulacao dos formatos dos arquivos.
    
    -application.yml: adicao das configs necessarias para conectar com a API da AWS.
    
    - S3Config: classe de configuracao do spring responsavel por retornar um bean com as configs declaradas no arquivo application.yml
    
    - S3Service: classe de servico responsavel por conter os metodos e regras de negocio utilizados pela applicacao para manipulacao das imagens no bucket.
    
    - ProjetoSpringBootApplication: injetada uma depedencia da classe S3Service e declarado o caminho de um arquivo local de forma hardcoded apenas para teste rapido ao iniciar a applicacao.

--- Codigos Fonte/Exemplos sobre topicos/Back-end/ProjetoSpringBoot/ProjetoSpringBoot/src/main/java/br/com/estudos/springboot/projetospringboot/ProjetoSpringBootApplication.java
+++ Codigos Fonte/Exemplos sobre topicos/Back-end/ProjetoSpringBoot/ProjetoSpringBoot/src/main/java/br/com/estudos/springboot/projetospringboot/ProjetoSpringBootApplication.java
@@ -4,6 +4,7 @@ import br.com.estudos.springboot.projetospringboot.domain.*;
 import br.com.estudos.springboot.projetospringboot.domain.enums.EstadoPagamento;
 import br.com.estudos.springboot.projetospringboot.domain.enums.TipoCliente;
 import br.com.estudos.springboot.projetospringboot.ropository.*;
+import br.com.estudos.springboot.projetospringboot.service.S3Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,12 +17,15 @@ import java.util.Arrays;
 @SpringBootApplication
 public class ProjetoSpringBootApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoSpringBootApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		s3Service.uploadFile("/home/trajy/Imagens/3ds.jpg");
 	}
 }
