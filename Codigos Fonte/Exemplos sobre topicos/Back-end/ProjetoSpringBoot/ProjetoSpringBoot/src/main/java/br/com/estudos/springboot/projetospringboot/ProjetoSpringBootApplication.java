97e00a37f97bda0c7d3afba7affa518dfecc887f
Henrique Trajano de Araujo <henrique.trajano.a@gmail.com>
2022-05-16 11:25:29 -0300

    remocao-teste-rapido-de-envio-para-s3-ao-iniciar-a-aplicacao

--- Codigos Fonte/Exemplos sobre topicos/Back-end/ProjetoSpringBoot/ProjetoSpringBoot/src/main/java/br/com/estudos/springboot/projetospringboot/ProjetoSpringBootApplication.java
+++ Codigos Fonte/Exemplos sobre topicos/Back-end/ProjetoSpringBoot/ProjetoSpringBoot/src/main/java/br/com/estudos/springboot/projetospringboot/ProjetoSpringBootApplication.java
@@ -1,24 +1,11 @@
 package br.com.estudos.springboot.projetospringboot;
 
-import br.com.estudos.springboot.projetospringboot.domain.*;
-import br.com.estudos.springboot.projetospringboot.domain.enums.EstadoPagamento;
-import br.com.estudos.springboot.projetospringboot.domain.enums.TipoCliente;
-import br.com.estudos.springboot.projetospringboot.ropository.*;
-import br.com.estudos.springboot.projetospringboot.service.S3Service;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
 
 @SpringBootApplication
 public class ProjetoSpringBootApplication implements CommandLineRunner {
-
-	@Autowired
-	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoSpringBootApplication.class, args);
@@ -26,6 +13,6 @@ public class ProjetoSpringBootApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("/home/trajy/Imagens/3ds.jpg");
+
 	}
 }
